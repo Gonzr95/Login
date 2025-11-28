c3508817138dd51aa029b028cf19b84d6a1106e9
Gonza PC - Temperley <gonz.r@hotmail.com>
2025-11-28 20:09:25 -0300

    Add user registration validation schema and middleware

--- routes/users.js
+++ routes/users.js
@@ -1,11 +1,11 @@
 import { Router } from "express";
-import { 
-  register
-} from "../controllers/user.js";
+import { register } from "../controllers/user.js";
+import { validateSchema } from '../middlewares/validator.js'; // El middleware
+import { registerUserSchema } from '../schemas/user.js'; // El esquema
 
 const router = Router();
 
-router.post("/users", register);
+router.post("/users", validateSchema(registerUserSchema), register);
 
 
 export {router};
---
+++ schemas/user.js
@@ -0,0 +1,22 @@
+import { z } from 'zod';
+
+export const registerUserSchema = z.object({
+    firstName: z.string({
+        required_error: "El nombre es obligatorio",
+        invalid_type_error: "El nombre debe ser un texto"
+    }).min(2, { message: "El nombre debe tener al menos 2 caracteres" }),
+
+    lastName: z.string({
+        required_error: "El apellido es obligatorio"
+    }).min(2, { message: "El apellido debe tener al menos 2 caracteres" }),
+
+    mail: z.string({
+        required_error: "El email es obligatorio",
+    })
+    .email("Formato de email inválido") // 👈 Mensaje de error directamente como argumento de .email()
+    .nonempty("El email es obligatorio"), // 👈 Opcional: para asegurar que no sea un string vacío
+
+    pass: z.string({
+        required_error: "La contraseña es obligatoria"
+    }).min(6, { message: "La contraseña debe tener al menos 6 caracteres" })
+});
